1058fa4c848cdee34b392b6b2d1218bdee913cef
Euan Gaffney <euangaffney@mobile-mbp.3squared>
2019-01-08 09:17:58 +0000

    Initial Commit

---
+++ Food Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Euan Gaffney on 08/01/2019.
+//  Copyright © 2019 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
